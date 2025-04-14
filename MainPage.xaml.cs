8804c7775f41c0d3eec69bea7ec41284ef37ead4
borisbvf <borisbvf@outlook.com>
2025-04-14 16:28:21 +0900

    Change app icon and splash screen. Add images for the main button.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -13,11 +13,41 @@ namespace Flashlight
 
 		public LocalizationManager LocalizationManager => LocalizationManager.Instance;
 
+        private bool buttonOn = false;
+
+        private string imageSource = "buttonoff.png";
+        public string ImageSource
+        {
+            get => imageSource;
+            set
+            {
+                if (value != imageSource)
+                {
+                    imageSource = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
 		public ICommand SettingsCommand => new Command(GoToSettings);
         private async void GoToSettings()
         {
             await Shell.Current.GoToAsync($"{Constants.SettingsPageRoute}");
         }
-    }
+
+        public ICommand TurnOnOffCommand => new Command(TurnOnOff);
+        private async void TurnOnOff()
+        {
+            if (buttonOn)
+            {
+                ImageSource = "buttonoff.png";
+            }
+            else
+            {
+                ImageSource = "buttonon.png";
+            }
+            buttonOn = !buttonOn;
+        }
+	}
 
 }
